32c99291a2c33ae96c4d603f654aa222151b2fb6
Coocko <krabik4794@gmail.com>
2021-11-17 23:31:31 +0200

    django part 7

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Hello
-admin.site.register(Hello)
+from .models import Question, Choice
+admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
-# Register your models here.
-#djangp 4 part
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
